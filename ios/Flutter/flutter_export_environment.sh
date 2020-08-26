b47f0163376523904b005ed590c61ae16355a7bc
Güvenç Atlı <atli15@itu.edu.tr>
2020-08-26 16:00:38 +0300

    first

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\seyfu\Desktop\codecanyon-3hmcfFCB-grocery-food-pharmacy-store-delivery-mobile-app-with-admin-panel\flutter_application"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.5.0"
+export "FLUTTER_BUILD_NUMBER=1.5.0"
